ec9c1e02618108b00ed7775b1ee87c2a8c8e840e
Alexander Vasnev <a-s-vasnev88@yandex.ru>
2022-10-12 20:55:34 +0300

    change homework

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -6,9 +6,13 @@ const successClose = document.querySelector('#modal_success .modal__close');
 
 modal.classList.add('modal_active');
 
-showSuccess.onclick = () => modalSuccess.classList.add('modal_active');
-modalClose.onclick = () => modal.classList.remove('modal_active');
-successClose.onclick = () => {
+showSuccess.onclick = () => {
 	modal.classList.remove('modal_active');
+	modalSuccess.classList.add('modal_active');
+}
+
+modalClose.onclick = () => modal.classList.remove('modal_active');
+
+successClose.onclick = () => {
 	modalSuccess.classList.remove('modal_active');
 }
