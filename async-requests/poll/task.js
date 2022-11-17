0f987fe924ddffc0411fb98f857d9e7bc67c4356
Alexander Vasnev <a-s-vasnev88@yandex.ru>
2022-11-17 14:10:46 +0300

    add function AJAX

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -1,35 +1,28 @@
 const pollAnswers = document.querySelector('#poll__answers');
 const pollTitle = document.querySelector('#poll__title');
 
-const xhrPoll = new XMLHttpRequest();
-xhrPoll.open('GET', "https://netology-slow-rest.herokuapp.com/poll.php");
-xhrPoll.responseType = 'json';
-xhrPoll.send();
-
-xhrPoll.onload = () => {
+const xhrPoll = getXHR('GET', "https://netology-slow-rest.herokuapp.com/poll.php");
+xhrPoll.addEventListener('load', () => {
 	pollTitle.innerText = xhrPoll.response.data.title;
 	xhrPoll.response.data.answers.find(item => {
 		pollAnswers.innerHTML += `
 			<button class="poll__answer">
-              ${item}
-            </button>
+			${item}
+			</button>
 		`;
 	})
 	const pollAnsver = document.getElementsByClassName('poll__answer');
 	for(let i = 0; i < pollAnsver.length; i++) {
 		pollAnsver[i].onclick = () => {
-            alert('«Спасибо, ваш голос засчитан!»');
-            showStatic(pollAnsver[i]);
-        }
+			alert('«Спасибо, ваш голос засчитан!»');
+			showStatic(pollAnsver[i]);
+		}
 	}
-}
+})
+
 
 function showStatic(index) {
-	const xhrAnswer = new XMLHttpRequest();
-	xhrAnswer.open( 'POST', 'https://netology-slow-rest.herokuapp.com/poll.php' );
-	xhrAnswer.setRequestHeader( 'Content-type', 'application/x-www-form-urlencoded' );
-	xhrAnswer.responseType = 'json';
-	xhrAnswer.send( `vote=${xhrPoll.response.id}&answer=${index}` );
+	const xhrAnswer = getXHR('POST', 'https://netology-slow-rest.herokuapp.com/poll.php', `vote=${xhrPoll.response.id}&answer=${index}`);
 	xhrAnswer.onload = () => {
 		pollAnswers.innerHTML = '';
 		xhrAnswer.response.stat.find(item => {
@@ -37,3 +30,30 @@ function showStatic(index) {
 		})
 	}
 }
+
+function getXHR(method, url, data = null) {
+	const xhr = new XMLHttpRequest();
+	xhr.open(method, url);
+	xhr.setRequestHeader( 'Content-type', 'application/x-www-form-urlencoded' );
+	xhr.responseType = 'json';
+	xhr.send(data);
+	return xhr;
+}
+
+// function addButton() {
+// 	pollTitle.innerText = xhrPoll.response.data.title;
+// 	xhrPoll.response.data.answers.find(item => {
+// 		pollAnswers.innerHTML += `
+// 			<button class="poll__answer">
+// 			${item}
+// 			</button>
+// 		`;
+// 	})
+// 	const pollAnsver = document.getElementsByClassName('poll__answer');
+// 	for(let i = 0; i < pollAnsver.length; i++) {
+// 		pollAnsver[i].onclick = () => {
+// 			alert('«Спасибо, ваш голос засчитан!»');
+// 			showStatic(pollAnsver[i]);
+// 		}
+// 	}
+// }
